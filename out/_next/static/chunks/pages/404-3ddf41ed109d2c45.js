dce5540abb1c701c1e4f4360db0e4c0db6a6c662
huzan2 <huzan2@naver.com>
2023-08-13 18:01:53 +0900

    deploy to gh-pages

---
+++ out/_next/static/chunks/pages/404-3ddf41ed109d2c45.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[197],{394:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/404",function(){return t(1604)}])},1604:function(n,u,t){"use strict";function _(){return"[404]Invalid URL"}t.r(u),t.d(u,{default:function(){return _}})}},function(n){n.O(0,[774,888,179],function(){return n(n.s=394)}),_N_E=n.O()}]);
